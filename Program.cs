90e8d5c1f3683631e8568b2eea4477e700d93579
Shirish Shrestha <shrestha_sl@hotmail.com>
2019-11-15 12:18:37 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> numbers = new List<int>
+            {
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6)
+            };
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers.Contains(i))
+                {
+                    Console.WriteLine($"numbers list contains {i}");
+                }
+                else
+                {
+                    Console.WriteLine($"numbers list does not contains {i}");
+                }
+            }
+        }
+    }
+}
